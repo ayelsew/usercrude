89790fcdddf22ee56a27315da7339575860ac97d
leydev <wesley.waaraujo@gmail.com>
2020-05-24 22:47:02 -0300

    Criaçao dos componentes table e modal

---
+++ src/components/Modal.vue
@@ -0,0 +1,153 @@
+<template>
+  <div class="bg-modal" v-bind:class="{ hide: hideModal }">
+    <div class="modal">
+      <h3 class="modal-title">{{ title }}</h3>
+      <form action>
+        <div class="flex-direction-column">
+          <label for="InputName">Nome</label>
+          <input
+            v-bind:value="name"
+            @input="setName"
+            placeholder="Ex: Wesley"
+            id="InputName"
+            type="text"
+          />
+        </div>
+        <div class="flex-direction-column">
+          <label for="InputEmail">E-mail</label>
+          <input
+            @input="setEmail"
+            placeholder="Ex: wesley@safadao.com"
+            v-bind:value="email"
+            id="InputEmail"
+          />
+          <small></small>
+        </div>
+        <div class="flex-direction-column">
+          <label for="InputRole">Cargo</label>
+          <input
+            @input="setRole"
+            placeholder="Ex: Fullstack"
+            v-bind:value="role"
+            id="InputRole"
+            type="text"
+          />
+        </div>
+        <div class="flex-direction-row justify-content-space-between">
+          <button v-on:click="button0" type="button" class="btn-model bg-red">Cancelar</button>
+          <button v-on:click="button1" type="button" class="btn-model bg-black">Gravar</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  computed: {
+    hideModal() {
+      return this.$store.state.modal.hide;
+    },
+    title() {
+      return this.$store.state.modal.title;
+    },
+    name() {
+      return this.$store.state.modal.name;
+    },
+    email() {
+      return this.$store.state.modal.email;
+    },
+    role() {
+      return this.$store.state.modal.role;
+    }
+  },
+  methods: {
+    setName(input) {
+      const d = input.target.value;
+      this.$store.dispatch("setModalName", d);
+    },
+    setEmail(input) {
+      const d = input.target.value;
+      this.$store.dispatch("setModalEmail", d);
+    },
+    setRole(input) {
+      const d = input.target.value;
+      this.$store.dispatch("setModalRole", d);
+    },
+    button0() {
+      this.$store.state.modal.button0.func();
+    },
+    button1() {
+      this.$store.state.modal.button1.func();
+    }
+  }
+};
+</script>
+
+<style scoped>
+.bg-modal {
+  background-color: #d5d5d566;
+  height: 100%;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.modal {
+  background-color: #ffffff;
+  padding: 1.5rem;
+  box-shadow: 0 0px 18px 0px gray;
+  border-radius: 1px;
+  min-width: 20rem;
+  min-height: 10rem;
+}
+
+.modal > .modal-title {
+  margin: 0;
+}
+
+.modal .btn-model {
+  border: unset;
+  padding: 0.5rem 2.9rem;
+  cursor: pointer;
+  font-weight: 900;
+  color: #ffffff;
+  text-transform: uppercase;
+}
+
+.flex-direction-column {
+  display: flex;
+  flex-direction: column;
+  margin: 20px 0px;
+}
+.flex-direction-row {
+  display: flex;
+  flex-direction: row;
+  margin: 20px 0px;
+}
+
+.justify-content-space-between {
+  justify-content: space-between;
+}
+
+label {
+  margin-bottom: 1px;
+  margin-bottom: 4px;
+}
+
+.bg-black {
+  background-color: #2d2d2d;
+}
+.bg-red {
+  background-color: #e28686;
+}
+
+.hide {
+  display: none;
+}
+</style>
---
+++ src/components/Table.vue
@@ -0,0 +1,162 @@
+<template>
+  <table id="UsersTable" cellspacing="0" cellpadding="0">
+    <thead>
+      <th>#</th>
+      <th>Nome</th>
+      <th>E-mail</th>
+      <th>Cargo</th>
+    </thead>
+    <tbody>
+      <tr v-if="users.length === 0">
+        <td colspan="5">Não há dados</td>
+      </tr>
+      <template v-for="(user,index) in users">
+        <tr v-bind:key="index">
+          <td v-if="index+1 < 10">{{"0"+(index+1)}}</td>
+          <td v-else>{{index+1}}</td>
+          <td>{{user.name.split(" ")[0]}}</td>
+          <td>{{user.email}}</td>
+          <td>{{user.role}}</td>
+          <td>
+            <button class="btn-table" v-on:click="editUser(index)">Alt</button>
+            <button class="btn-table" v-on:click="deleteUser(user.id)">Del</button>
+          </td>
+        </tr>
+      </template>
+    </tbody>
+    <tfoot>
+      <tr>
+        <td colspan="5">
+          <button v-on:click="addUser" class="btn-table">Adicionar usuário</button>
+        </td>
+      </tr>
+    </tfoot>
+  </table>
+</template>
+
+<script>
+import { db } from "../db";
+
+export default {
+  name: "Table",
+  data: () => ({
+    users: []
+  }),
+  firestore: {
+    users: db.collection("users").orderBy("createdAt")
+  },
+  methods: {
+    addUser() {
+      const { dispatch } = this.$store;
+      const cleanUp = () => {
+          dispatch("setModalTitle", "")
+          dispatch("setModalName", "");
+          dispatch("setModalEmail", "")
+          dispatch("setModalRole", "")
+      }
+      dispatch("setModalTitle", "ADICIONAR USUÁRIO");
+      dispatch("setModalButton0", {
+        func: () => {
+          dispatch("closeModal");
+          cleanUp();
+        }
+      });
+      dispatch("setModalButton1", {
+        func: () => {
+          const { modal } = this.$store.state;
+          const user = {
+            name: modal.name.trim(),
+            email: modal.email,
+            role: modal.role,
+            createdAt: Date.now()
+          };
+          db.collection("users").add(user);
+          dispatch("closeModal");
+          cleanUp();
+        }
+      });
+      this.$store.dispatch("openModal");
+    },
+    editUser(i) {
+      const user = this.users[i];
+      const { dispatch } = this.$store;
+      const { modal } = this.$store.state;
+
+      dispatch("setModalTitle", `Editar usuário #${i + 1}`);
+      dispatch("setModalName", user.name);
+      dispatch("setModalEmail", user.email);
+      dispatch("setModalRole", user.role);
+
+      let func0 = () => dispatch("closeModal"),
+        func1 = () => {
+          db.collection("users")
+            .doc(user.id)
+            .update({
+              name: modal.name,
+              email: modal.email,
+              role: modal.role
+            });
+          dispatch("closeModal");
+        };
+
+      dispatch("setModalButton0", {
+        func: func0
+      });
+
+      dispatch("setModalButton1", {
+        func: func1
+      });
+      dispatch("openModal");
+    },
+    deleteUser(id) {
+      db.collection("users")
+        .doc(id)
+        .delete();
+    }
+  }
+};
+</script>
+
+<style scoped>
+table#UsersTable {
+  width: 70%;
+  margin: auto;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+
+table#UsersTable .btn-table {
+  padding: 0;
+  margin: 0;
+  border-style: solid;
+  background: #2f2f2f;
+  color: white;
+  border-color: #2f2f2f;
+  border-radius: 4px;
+  border-width: 0.3rem 0.5rem;
+  cursor: pointer;
+}
+
+table#UsersTable tr {
+  height: 2.5rem;
+  text-align: start;
+}
+table#UsersTable > tbody > tr > td:last-child {
+  text-align: center;
+}
+table#UsersTable > tbody > tr > td:last-child > button {
+  margin: 0 0.2rem;
+}
+
+table#UsersTable > tbody > tr:nth-child(2n + 1) {
+  background: #f3f3f3;
+}
+
+table#UsersTable > tfoot > * {
+  text-align: center;
+  font-weight: 600;
+}
+table#UsersTable > tfoot > tr > td > button {
+  text-align: center;
+  font-weight: 600;
+}
+</style>
